f70a831b20919f8a69c1ca18cffd55c7046ec093
yash4137 <23cs011@charusat.edu.in>
2025-11-28 21:21:43 +0530

    feat: add user authentication and loading state to Credits page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,12 @@ import Community from './pages/Community'
 import { assets } from './assets/assets'
 import './assets/prism.css'
 import Loading from './pages/Loading'
+import { useAppContext } from './context/AppContext'
+import Login from './pages/Login'
 
 const App = () => {
+
+  const {user} = useAppContext();
 
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const {pathname} = useLocation();
@@ -20,6 +24,8 @@ const App = () => {
   return (
     <>
     {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={() => setIsMenuOpen(true)} />}
+
+    {user ? (
       <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
         <div className='flex h-screen w-screen'>
           <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
@@ -30,6 +36,10 @@ const App = () => {
           </Routes>
         </div>
       </div>
+    ) : (
+      <div className='bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'><Login /></div>
+    )}
+
     </>
   )
 }
