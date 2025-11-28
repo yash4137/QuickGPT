995072be00f016c9094ccd9f0e8bc80469fd3c87
yash4137 <23cs011@charusat.edu.in>
2025-11-28 18:34:16 +0530

    feat: implement loading page and enhance community image fetching

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useRef, useState } from 'react'
 import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
 import Message from './Message'
 
 const ChatBox = () => {
+
+  const containerRef = useRef(null)
 
   const {selectedChat, theme} = useAppContext()
 
@@ -24,11 +26,20 @@ const ChatBox = () => {
     }
   }, [selectedChat])
 
+  useEffect(()=>{
+    if(containerRef.current){
+      containerRef.current.scrollTo({
+        top: containerRef.current.scrollHeight,
+        behavior: 'smooth',
+      })
+    }
+  }, [messages])
+
   return (
     <div className="flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40">
 
       {/* Chat Messages */}
-      <div className='flex-1 mb-5 overflow-y-scroll'>
+      <div ref={containerRef} className='flex-1 mb-5 overflow-y-scroll'>
         {messages.length === 0 && (
           <div className='h-full flex flex-col items-center justify-center gap-2 text-primary'>
             <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt="" className="w-full max-w-56 sm:max-w-68"/>
@@ -47,6 +58,13 @@ const ChatBox = () => {
           </div>
         }
       </div>
+
+      {mode === 'image' && (
+        <label className='inline-flex items-center gap-2 mb-3 text-sm mx-auto'>
+          <p className='text-xs'>Publish Generated Image to community</p>
+          <input type="checkbox" className='cursor-pointer' checked={isPublished} onChange={(e) => setIsPublished(e.target.checked)} />
+        </label>
+      )}
 
       {/* Prompt Input Box */}
       <form onSubmit={onSubmit} className='bg-primary/20 dark:bg-[#583C79]/30 border border-primary dark:border-[#80609F]/30 rounded-full w-full max-w-2xl p-3 pl-4 mx-auto flex gap-4 items-center'>
